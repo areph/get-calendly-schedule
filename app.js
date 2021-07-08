e1d3b8ddc883a73143a7d86921151d21cdbd8cda
areph <40balmung@gmail.com>
2021-07-08 22:36:52 +0900

    set command line args for calndly api

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
 'use strict';
 
-const commander = require('commander');
+const program = require('commander');
+const superagent = require('superagent');
 
-commander.parse(process.argv);
-console.log(commander.args[0]);
+program
+  .version('0.0.1')
+  .requiredOption('-t, --event_type <type>', 'set calendly event_type')
+  .requiredOption('-s, --start_date <type>', 'set startdate ex) 2021-07-11')
+  .requiredOption('-e, --end_date <type>', 'set enddate ex) 2021-07-12')
+  .parse(process.argv);
+
+const options = program.opts();
+if (!options.event_type || !options.start_date || !options.end_date) {
+  return;
+}
+
+(async () => {
+  try {
+    const url = `https://calendly.com/api/booking/event_types/${options.event_type}/calendar/range`;
+    const query = {
+      timezone: 'Asia/Tokyo',
+      diagnostics: false,
+      range_start: options.start_date,
+      range_end: options.end_date,
+    };
+    const response = await superagent.get(url).query(query);
+    console.log(
+      JSON.parse(response.text)['days'][0]['spots'].flatMap(
+        (x) => x['start_time']
+      )
+    );
+  } catch (error) {
+    console.log(error.response.body);
+  }
+})();
